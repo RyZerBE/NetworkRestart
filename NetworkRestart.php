94dceaa3569ebb48497eee60c369eeafe1dc5ae6
Matze <47496465+Matze997@users.noreply.github.com>
2021-10-16 11:55:08 +0200

    Implement commands and some code improvements

--- NetworkRestart.php
+++ NetworkRestart.php
@@ -4,6 +4,9 @@ const STOP_SCRIPT = [
     "screen -S RyZerCloud -X stuff 'group stop all\n'",
     "screen -S Bungee -X stuff 'end\n'",
 ];
+const KILL_SCRIPTS = [
+    "screen -XS Bungee kill"
+];
 const RESTART_SCRIPT = [
     "screen -S RyZerCloud -X stuff 'group start all\n'",
     "cd /root/WaterdogPE/ && screen -AmdS Bungee java -jar WaterdogPE.jar",
@@ -12,26 +15,74 @@ const RESTART_SCRIPT = [
 $restartTime = "04:00";
 $restarted = false;
 
-echo "Starting loop...\n";
-echo "Current Time: " . date("H:i") . "\n";
+echo str_repeat("\n", 100);
+info("Loop started! Use /help for a list of commands");
+info("Current Time: " . date("H:i"));
 
 while(true) {
-    sleep(5);
+    sleep(1);
+
+    checkInput();
 
     $date = date("H:i");
     if($date === $restartTime) {
         if($restarted) continue;
-
-        $microtime = microtime(true);
-
-        echo "Restarting Network...\n";
-        foreach(STOP_SCRIPT as $script) popen($script, "r");
-        sleep(5);
-        foreach(RESTART_SCRIPT as $script) passthru($script);
-        echo "Done! Took " . round(microtime(true) - $microtime, 2) . "s\n";
-
+        restart();
         $restarted = true;
         continue;
     }
     $restarted = false;
+}
+
+function checkInput(): void {
+    $fh = @fopen("php://stdin", "r");
+    if($fh === false) return;
+    stream_set_blocking($fh, false);
+    $get = fgets($fh);
+    if($get === false) return;
+    $userInput = trim($get);
+    fclose($fh);
+    if(empty($userInput) || !str_starts_with($userInput, "/")) return;
+    $userInput = substr($userInput, 1);
+
+    switch($userInput) {
+        case "restart": {
+            restart();
+            break;
+        }
+        case "time": {
+            info("Current Time: " . date("H:i"));
+            break;
+        }
+        case "stop": {
+            exit();
+        }
+        case "help": {
+            $commandList = [
+                "restart",
+                "time",
+                "help",
+                "stop",
+            ];
+            foreach($commandList as $str) {
+                info("» /" . $str);
+            }
+            break;
+        }
+    }
+}
+
+function restart(): void {
+    $microtime = microtime(true);
+    info("Restarting Network...");
+    foreach(STOP_SCRIPT as $script) popen($script, "r");
+    sleep(5);
+    foreach(KILL_SCRIPTS as $script) popen($script, "r");
+    sleep(5);
+    foreach(RESTART_SCRIPT as $script) passthru($script);
+    info("Done! Took " . round(microtime(true) - $microtime, 2) . "s");
+}
+
+function info(string $log): void{
+    echo "[" . date("H:i") . "] " . $log . "\n";
 }
